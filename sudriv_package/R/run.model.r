149b605a2ab45a9df27a50c0ce86a87d0ed007f3
Lorenz Ammann <lorenz.ammann@eawag.ch>
2018-12-04 15:38:25 +0100

    Likelihood version
    It still needs the dummy input argument to the function run.model

--- sudriv_package/R/run.model.r
+++ sudriv_package/R/run.model.r
@@ -1,4 +1,4 @@
-run.model <- function(layout, sudriv){ ## par and layout are just dummy arguments to keep the function compatible with the general structure of the calling likelihood function. They are not used. Instead, the parameters and layout are taken from the sudriv object.
+run.model <- function(par, layout, sudriv){ ## par and layout are just dummy arguments to keep the function compatible with the general structure of the calling likelihood function. They are not used. Instead, the parameters and layout are taken from the sudriv object.
     res.sup <- run.engine(sudriv)
     ind.var <- result_index_var(res.sup=res.sup, file.o="outnames.txt", variables=unique(layout$layout[,1]), outnames=sudriv$model$outnames)
     y.mod   <- result2layout(res.sup=res.sup, ind.var=ind.var, layout=layout, meas.point.val=sudriv$settings$meas.point.val)
