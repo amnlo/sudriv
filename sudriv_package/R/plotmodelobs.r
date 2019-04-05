7d8fafe45ae686b5ce1a5de186fa6694d3526836
Lorenz Ammann <loam.eawag@gmail.com>
2019-04-05 18:14:33 +0200

    Minor: axis labels and translations
    Changed some translations of variables to have nicer names for plotting
    and some axis label adjustments.

--- sudriv_package/R/plotmodelobs.r
+++ sudriv_package/R/plotmodelobs.r
@@ -7,9 +7,10 @@ plot.results <- function(layout.mod, y.mod, layout.obs=NULL, y.obs=NA, vary=list
     ##     hcl(h = hues, l = 65, c = 100)[1:n]
     ## }
     scales <- scales[1]
+    strmflw.units <- ifelse(per.area,"(mm/15min)","l/s")
     ##translate.var <- c("C1Tc1_Qstream", "C1Tc2_Qstream", "C2Tc1_Qstream", "C2Tc2_Qstream", "C3Tc1_Qstream", "C3Tc2_Qstream", "C4Tc1_Qstream", "C4Tc2_Qstream", "C5Tc1_Qstream", "C5Tc2_Qstream")
-    translate.var <- c("C1Wv_Qstream", "C2Wv_Qstream", "C3Wv_Qstream", "C4Wv_Qstream", "C5Wv_Qstream", "C1Tc1_Qstream", "C1Tc2_Qstream", "C2Tc1_Qstream", "C2Tc2_Qstream", "C3Tc1_Qstream", "C3Tc2_Qstream", "C4Tc1_Qstream", "C4Tc2_Qstream", "C5Tc1_Qstream", "C5Tc2_Qstream", "U1F1Wv_Qstrm", "U2F1Wv_Qstrm", "U3F1Wv_Qstrm", "U4F1Wv_Qstrm", "U5F1Wv_Qstrm", "U1F1Tm1_Qstrm", "U2F1Tm1_Qstrm", "U3F1Tm1_Qstrm", "U4F1Tm1_Qstrm", "U5F1Tm1_Qstrm", "U1F1Tm2_Qstrm", "U2F1Tm2_Qstrm", "U3F1Tm2_Qstrm", "U4F1Tm2_Qstrm", "U5F1Tm2_Qstrm", "U1F1Wv_Qq_FR", "U2F1Wv_Q_RR", "U3F1Wv_Qq_FR", "HYPERSTATE_sorption")
-    translate.to <- c("C1Wv", "C2Wv", "C3Wv", "C4Wv", "C5Wv", "C1Tc1", "C1Tc2", "C2Tc1", "C2Tc2", "C3Tc1", "C3Tc2", "C4Tc1", "C4Tc2", "C5Tc1", "C5Tc2", "Impervious", "Shortcut", "Drained", "SC and Drained", "Rest", "Impervious", "Shortcut", "Drained", "SC and Drained", "Rest", "Impervious", "Shortcut", "Drained", "SC and Drained", "Rest", "ImperviousFR", "ShortcutRR", "DrainedFR", "Apparent~K[d]~(l/kg)")
+    translate.var <- c("C1Wv_Qstream", "C2Wv_Qstream", "C3Wv_Qstream", "C4Wv_Qstream", "C5Wv_Qstream", "C1Tc1_Qstream", "C1Tc2_Qstream", "C2Tc1_Qstream", "C2Tc2_Qstream", "C3Tc1_Qstream", "C3Tc2_Qstream", "C4Tc1_Qstream", "C4Tc2_Qstream", "C5Tc1_Qstream", "C5Tc2_Qstream", "U1F1Wv_Qstrm", "U2F1Wv_Qstrm", "U3F1Wv_Qstrm", "U4F1Wv_Qstrm", "U5F1Wv_Qstrm", "U1F1Tm1_Qstrm", "U2F1Tm1_Qstrm", "U3F1Tm1_Qstrm", "U4F1Tm1_Qstrm", "U5F1Tm1_Qstrm", "U1F1Tm2_Qstrm", "U2F1Tm2_Qstrm", "U3F1Tm2_Qstrm", "U4F1Tm2_Qstrm", "U5F1Tm2_Qstrm", "U1F1Wv_Qq_FR", "U2F1Wv_Q_RR", "U3F1Wv_Qq_FR", "U1F1Tm1_Qq_FR", "U2F1Tm1_Q_RR", "U3F1Tm1_Qq_FR", "U1F1Tm2_Qq_FR", "U2F1Tm2_Q_RR", "U3F1Tm2_Qq_FR", "U3F1MaT1_Si1Lv0", "U3F1MaT1_Si1Lv1", "U3F1MaT1_Si1Lv2", "U3F1MaT2_Si1Lv0", "U3F1MaT2_Si1Lv1", "U3F1MaT2_Si1Lv2", "HYPERSTATE_sorption")
+    translate.to <- c(bquote("Streamflow"~(.(strmflw.units))), "C2Wv", "C3Wv", "C4Wv", "C5Wv", expression("Atrazine"~(mu*g/l)), expression("Terbuthylazine"~(mu*g/l)), "C2Tc1", "C2Tc2", "C3Tc1", "C3Tc2", "C4Tc1", "C4Tc2", "C5Tc1", "C5Tc2", "Impervious", "Shortcut", "Drained", "SC and Drained", "Rest", "Impervious", "Shortcut", "Drained", "SC and Drained", "Rest", "Impervious", "Shortcut", "Drained", "SC and Drained", "Rest", "ImperviousFR", "ShortcutRR", "DrainedFR", "Impervious", "Shortcut", "Drainage",  "Impervious", "Shortcut", "Drainage", "Dissolved", "Fast sorbed", "Slow sorbed", "Dissolved", "Fast sorbed", "Slow sorbed", "Apparent~K[d]~(l/kg)")
     ##translate.to <- c(expression("St. 2, Atraz."~(mu*g/l)), expression("St. 2, Terb."~(mu*g/l)), expression("St. 4, Atraz."~(mu*g/l)), expression("St. 4, Terb."~(mu*g/l)))
     ##translate.to  <- c(expression(Atraz.~(mu*g/l)), expression(Terb.~(mu*g/l)), expression(Discharge~(l/s)), expression("P"~"(mm/15min)"))
     if(is.na(c(hru.areas)[1]) & !per.area) stop("hru.areas required if per.area is FALSE")
@@ -39,14 +40,6 @@ plot.results <- function(layout.mod, y.mod, layout.obs=NULL, y.obs=NA, vary=list
         }
         options("scipen"=0, "digits"=7)
     }
-    ## Translate variable names for plotting
-    y.dat$var <- as.factor(y.dat$var)
-    y.dat$vartrans <- y.dat$var
-    lv <- levels(y.dat$vartrans)
-    if(length(translate.to)>0){
-        lv[lv %in% translate.var] <- translate.to[match(lv[lv %in% translate.var], translate.var)]
-    }
-    levels(y.dat$vartrans) <- lv
     ## Convert streamflow from intesive to extensive quantity
     if(!per.area){
         cumulation <- list(c(1,2,3,4,5),
@@ -111,6 +104,14 @@ plot.results <- function(layout.mod, y.mod, layout.obs=NULL, y.obs=NA, vary=list
         y.dat.mass <- gather(y.dat.mass, var, y, -time)
         y.dat <- y.dat.mass
     }
+    ## Translate variable names for plotting
+    y.dat$var <- as.factor(y.dat$var)
+    y.dat$vartrans <- y.dat$var
+    lv <- levels(y.dat$vartrans)
+    if(length(translate.to)>0){
+        lv[lv %in% translate.var] <- translate.to[match(lv[lv %in% translate.var], translate.var)]
+    }
+    levels(y.dat$vartrans) <- lv
     ## Convert days in y.dat into time for plotting purposes
     strt <- as.POSIXct("2008-01-01")
     y.dat$time <- strt + as.numeric(y.dat$time * 60*60*24)
@@ -127,8 +128,8 @@ plot.results <- function(layout.mod, y.mod, layout.obs=NULL, y.obs=NA, vary=list
     }else if(masses){
     # stack the masses of tracer in the different reservoirs on top of each other
         yl <- expression("Mass ("*mu*g~m^{-2}*")")
-        ggplot.obj1 <- ggplot(subset(y.dat, grepl("T1", var)), aes(x=time, y=y, fill=var)) + geom_area() + labs(x="Time", y=yl, fill="", caption=paste("Half-life:",round(half.life),"days","Sorpt. coeff.: ", round(Kd,2),"l/kg"))
-        ggplot.obj2 <- ggplot(subset(y.dat, grepl("T2", var)), aes(x=time, y=y, fill=var)) + geom_area() + labs(x="Time", y=yl, fill="")
+        ggplot.obj1 <- ggplot(subset(y.dat, grepl("T1", var)), aes(x=time, y=y, fill=vartrans)) + geom_area() + labs(x="Time", y=yl, fill="", caption=paste("Half-life:",round(half.life),"days","Sorpt. coeff.: ", round(Kd,2),"l/kg"))
+        ggplot.obj2 <- ggplot(subset(y.dat, grepl("T2", var)), aes(x=time, y=y, fill=vartrans)) + geom_area() + labs(x="Time", y=yl, fill="")
         ggplot.obj <- gtable_rbind(ggplotGrob(ggplot.obj1), ggplotGrob(ggplot.obj2))
         dev.off()
         width = 15
@@ -146,7 +147,7 @@ plot.results <- function(layout.mod, y.mod, layout.obs=NULL, y.obs=NA, vary=list
             all.SR <- as.numeric(tapply(y.dat2[ind,"y"], as.factor(as.numeric(y.dat2[ind,"time"])), sum))
             y.dat3 <- y.dat2[one,]
             y.dat3$y <- all.SR
-            y.dat3$var <- "All_SR"
+            y.dat3$var <- "All groundwater"
             y.dat2[one,] <- y.dat3
             y.dat2 <- y.dat2[!(ind & !one),]
         }
@@ -163,8 +164,8 @@ plot.results <- function(layout.mod, y.mod, layout.obs=NULL, y.obs=NA, vary=list
         fluxes.multhru <- TRUE
         ggplot.obj <- list()
         if(sum(grepl("Wv", variables))>0)  ggplot.obj <- c(ggplot.obj, list(ggplotGrob(ggplot(subset(y.dat, grepl("Wv", var) | grepl("All",var)), aes(x=time, y=y, fill=vartrans)) + geom_area() + labs(x="Time", y="Streamflow contribution (l/s)", fill=""))))
-        if(sum(grepl("Tm1", variables))>0) ggplot.obj <- c(ggplot.obj, list(ggplotGrob(ggplot(subset(y.dat, grepl("Tm1", var)| grepl("All",var)), aes(x=time, y=y, fill=vartrans)) + geom_area() + labs(x="Time", y=expression("Load ("*mu*g*"/15min"), fill=""))))
-        if(sum(grepl("Tm2", variables))>0) ggplot.obj <- c(ggplot.obj, list(ggplotGrob(ggplot(subset(y.dat, grepl("Tm2", var) | grepl("All",var)), aes(x=time, y=y, fill=vartrans)) + geom_area() + labs(x="Time", y=expression("Load ("*mu*g*"/15min"), fill=""))))
+        if(sum(grepl("Tm1", variables))>0) ggplot.obj <- c(ggplot.obj, list(ggplotGrob(ggplot(subset(y.dat, grepl("Tm1", var)| grepl("All",var)), aes(x=time, y=y, fill=vartrans)) + geom_area() + labs(x="Time", y=expression("Load ("*mu*g*"/15min)"), fill=""))))
+        if(sum(grepl("Tm2", variables))>0) ggplot.obj <- c(ggplot.obj, list(ggplotGrob(ggplot(subset(y.dat, grepl("Tm2", var) | grepl("All",var)), aes(x=time, y=y, fill=vartrans)) + geom_area() + labs(x="Time", y=expression("Load ("*mu*g*"/15min)"), fill=""))))
         ggplot.obj <- do.call(gtable_rbind, ggplot.obj)
     }else{
         ggplot.obj <- ggplot(y.dat, aes(x=time, y=y, shape=modobs, col=modobs)) + scale_x_datetime(limits=xlim) + scale_y_continuous(limits=ylim)
