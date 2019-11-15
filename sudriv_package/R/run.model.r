0676f81ab7f2635ef2be1cbbb8ac3a1d3183c545
Lorenz Ammann <lorenz.ammann@eawag.ch>
2019-11-15 18:11:46 +0100

    Efficiency gain
    Instance where object was grown inside a loop is now avoided.

--- sudriv_package/R/run.model.r
+++ sudriv_package/R/run.model.r
@@ -5,13 +5,9 @@ run.model <- function(layout, sudriv, lump=TRUE){ ## par and layout are just dum
     if(any(grepl("HYPERSTATE", var))){#dealing with hyperstates that are not directly calculated by the model, but they are calculated here based on the output of the model
         if(is.null(sudriv$model$hyperfun)) stop("hyperfun not defined, but found hyperstates")
         hyps <- unique(var[grepl("HYPERSTATE", var)])
-        j <- 1
-        for(hyp.curr in hyps){
-            result.hyp <- sudriv$model$hyperfun[[j]](res.sup, sudriv)
-            res.sup$y <- c(res.sup$y, result.hyp) # add the calculated output to the result vector, as if it was calculated by superflex
-            outnames <- c(outnames, hyp.curr) # adapt the outnames accordingly
-            j <- j + 1
-        }
+        result.hyp <- unlist(lapply(X=sudriv$model$hyperfun, FUN = function(f,a,b) f(a,b), a=res.sup,b=sudriv))
+        outnames <- c(outnames, hyps) # adapt the outnames accordingly
+        res.sup$y <- c(res.sup$y, result.hyp) # add the calculated output to the result vector, as if it was calculated by superflex
     }
     ind.var <- result_index_var(res.sup=res.sup, file.o="outnames.txt", variables=var, outnames=outnames)
     y.mod   <- result2layout(res.sup=res.sup, ind.var=ind.var, layout=layout, meas.point.val=sudriv$settings$meas.point.val, lump=lump)
